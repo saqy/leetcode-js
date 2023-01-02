a127971f824133d07a61f139c6db4b79fa629529
saqy <muhammadsaqiib@gmail.com>
2023-01-02 17:37:49 +0000

    fixes one last failing test

--- 392. Is Subsequence - isSubsequence.js
+++ 392. Is Subsequence - isSubsequence.js
@@ -3,20 +3,22 @@
  * @param {string} t
  * @return {boolean}
  */
- var isSubsequence = function(s, t) {
-    let j=0;
-    for (let i=0; i<t.length; i++){
-        if(t[i] === s[j]){
-            j++
-        }
-        if(j === s.length){
-            return true;
-        }
-
+var isSubsequence = function (s, t) {
+  if (t === '' && s === '') {
+    return true
+  }
+  let j = 0
+  for (let i = 0; i < t.length; i++) {
+    if (t[i] === s[j]) {
+      j++
     }
+    if (j === s.length) {
+      return true
+    }
+  }
 
-    return false
-};
+  return false
+}
 
 //time complexity: 0(n) since we are suing only one loop
 // complexity: 0(1) since we are using only one variable to store index;
