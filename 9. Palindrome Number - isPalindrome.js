1952a52c2f20df68d39393d3a903d8c459c71c61
saqy <muhammadsaqiib@gmail.com>
2023-01-05 22:23:07 +0000

    adds another solution for isPalindrome number

--- 9. Palindrome Number - isPalindrome.js
+++ 9. Palindrome Number - isPalindrome.js
@@ -2,11 +2,24 @@
  * @param {number} x
  * @return {boolean}
  */
- var isPalindrome = function(x) {
-    if (x.toString() === x.toString().split('').reverse().join('')){
-        return true;
-    }
-     return false;
-};
+//  var isPalindrome = function(x) {
+//     if (x.toString() === x.toString().split('').reverse().join('')){
+//         return true;
+//     }
+//      return false;
+// };
 
-// Time comlexity is 0(n), which can be improved 
+var isPalindrome = function (x) {
+  let reversedNumber = 0
+
+  const orignialNumber = x
+  while (x > 0) {
+    const lastDigit = x % 10
+    reversedNumber = reversedNumber * 10 + lastDigit
+    x = Math.floor(x / 10)
+  }
+
+  return reversedNumber === orignialNumber
+}
+
+// Time comlexity is 0(n)
